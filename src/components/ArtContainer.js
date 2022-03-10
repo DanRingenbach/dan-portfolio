8f090516ae8f2e371b09ae10367bb6f1412f0686
Daniel Ringenbach <danringenbach.art@gmail.com>
2022-03-09 23:24:28 -0500

    art page edits and button edits

--- src/components/ArtContainer.js
+++ src/components/ArtContainer.js
@@ -8,6 +8,8 @@ import Modal from 'react-bootstrap/Modal'
 
 function ArtContainer(props) {
 
+    
+
     const [show, setShow] = useState(false)
 
     const handleClose = () => setShow(false)
@@ -16,7 +18,7 @@ function ArtContainer(props) {
     return (
         <>
 
-            <div className='col-sm-6 art-card-container mt-5'>
+            <div  className='col-sm-6 art-card-container mt-5'>
                 <div style={{ maxWidth: '400px', minHeight: '400px' }} className=" m-3">
                     <div className=" art-card">
                         <h3 className="card-title">
@@ -24,7 +26,7 @@ function ArtContainer(props) {
                         </h3>
                         <img onClick={handleShow} id='img'
                             src={props.img}
-                            style={{ maxHeight: "300px", maxWidth: "300px", cursor: 'pointer' }}
+                            style={{ maxHeight: "100%", maxWidth: "100%", cursor: 'pointer' }}
                         ></img>
                         <p></p>
                         <h6>{props.medium}</h6>
@@ -35,10 +37,10 @@ function ArtContainer(props) {
             </div>
 
 
-            <Modal aria-labelledby='contained-modal-title-vcenter' centered id='artModal' show={show} onHide={handleClose} animation={false} class='modal'>
-                <Modal.Header closeButton></Modal.Header>
+            <Modal style={{ display: 'flex', justifyContent:'center'}}   id='artModal' show={show} onHide={handleClose} animation={true} class='modal'>
                 
-                <img src={props.img}></img>
+                
+                <img style={{ height: "100%", width: "100%"}} src={props.img}></img>
             </Modal>
         </>
     )
--- src/components/artworkData.js
+++ src/components/artworkData.js
@@ -1,23 +1,76 @@
 
-import Tree1 from './images/artWork/Tree1.png'
+// import Tree1 from './images/artWork/Tree1.png'
 import untitled1Img from './images/artWork/Untitled.png'
-import comeOnOver from './images/artWork/comeOnOver.png'
-import dream from './images/artWork/dream.jpg'
+// import comeOnOver from './images/artWork/comeOnOver.png'
+// import dream from './images/artWork/dream.jpg'
+import jamesDean from './images/artWork/jamesDean.jpg'
+import mondays from './images/artWork/mondays.jpg'
+import davidAndGoliath from './images/artWork/davidAndGoliath.jpg'
+import materialStudy from './images/artWork/materialStudy.png'
+import materialStudy2 from './images/artWork/materialStudy2.png'
+import badMood from './images/artWork/badMood.png'
+import motivationalSketch from './images/artWork/motivationalSketch.png'
 
 
 const artData = [
     {
         title: "Untitled",
         img: untitled1Img,
-        medium: "Acrylic - Butcher Paper",
-        descr: "This piece uses typography and color as well as the natural effect of aging on materials to reflect on the inevitble end of relationships."
+        medium: "acrylic - butcher paper",
+        descr: "This piece uses typography and color as well as the natural effect of aging on materials to reflect on the inevitble end of relationships"
+    },
+    // {
+    //     title: 'I Dream of You',
+    //     img: dream,
+    //     medium: 'Acrylic - Recycled Poster in Frame',
+    //     descr: 'This piece is a love letter written using letters as objects and through typography sends a message that would not normally be comprehensible in normal prose.'
+    // },
+    
+    {
+        title: 'Mondays',
+        img: mondays,
+        medium: 'acrylic - sketchbook paper',
+        descr: 'A sketch containing an amalgamation of symbols foreshadowing the end of the weekend'
     },
     {
-        title: 'I Dream of You',
-        img: dream,
-        medium: 'Acrylic - Recycled Poster in Frame',
-        descr: 'This piece is a love letter written using letters as objects and through typography sends a message that would not normally be comprehensible in normal prose.'
+        title: 'David and Goliath',
+        img: davidAndGoliath,
+        medium: 'acrylic - sketchbook paper',
+        descr:'A love letter to in the forma of a graph picturing a stylized volkswagen'
     },
+    {
+        title: 'Material Study 1',
+        img: materialStudy,
+        medium: 'newsprint - monoprint',
+        descr: 'Material study of aged newsprint'
+    },
+    {
+        title: 'Material Study 2',
+        img: materialStudy2,
+        medium: 'newsprint - monoprint - charcoal',
+        descr: 'Material study on aged newsprint with gestural charcoal markings'
+    },
+    {
+        title: 'James Dean',
+        img: jamesDean,
+        medium: 'cardboard - oil paint',
+        descr: 'Symbolic cypher with oil paint on cardboard. A combination of materials that degrades quickly and permanently'
+    },
+    {
+        title:'Material Study (BAD MOOD)',
+        img: badMood,
+        medium: 'newsprint - charcoal',
+        descr: 'Material study on aged newsprint with gestural charcoal markings including the text "BAD MOOD"'
+    },
+    {
+        title: 'Motivational Sketch',
+        img: motivationalSketch,
+        medium: 'newsprint - charcoal',
+        descr: 'Sketch on newsprint reminding the viewer to stay positive'
+    }
+
+
+
 
 
 ]
