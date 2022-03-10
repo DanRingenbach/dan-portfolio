ac157791f84ec535794c584b7beb8d1fe69f4376
Daniel Ringenbach <danringenbach.art@gmail.com>
2022-03-10 12:22:51 -0500

    modal edits

--- src/components/ArtContainer.js
+++ src/components/ArtContainer.js
@@ -8,25 +8,28 @@ import Modal from 'react-bootstrap/Modal'
 
 function ArtContainer(props) {
 
-    
+    const [loaded, setLoaded] = useState(false)
+
 
     const [show, setShow] = useState(false)
-
     const handleClose = () => setShow(false)
     const handleShow = () => setShow(true)
 
     return (
         <>
 
-            <div  className='col-sm-6 art-card-container mt-5'>
+            <div className='col-sm-6 art-card-container mt-5'>
                 <div style={{ maxWidth: '400px', minHeight: '400px' }} className=" m-3">
+
                     <div className=" art-card">
                         <h3 className="card-title">
                             {props.title}
                         </h3>
+
                         <img onClick={handleShow} id='img'
+                            style={loaded ? { maxHeight: "100%", maxWidth: "100%", cursor: 'pointer' } : { display: 'none' }}
                             src={props.img}
-                            style={{ maxHeight: "100%", maxWidth: "100%", cursor: 'pointer' }}
+                            onLoad={() => setLoaded(true)}
                         ></img>
                         <p></p>
                         <h6>{props.medium}</h6>
@@ -37,11 +40,22 @@ function ArtContainer(props) {
             </div>
 
 
-            <Modal style={{ display: 'flex', justifyContent:'center'}}   id='artModal' show={show} onHide={handleClose} animation={true} class='modal'>
+            <Modal centered style={loaded ? { display:'none' } : {display: 'none'}} 
+                   id='artModal' 
+                   show={show} 
+                   onHide={handleClose} 
+                   animation={true} 
+                   class='modal'
+                   onLoad={show}>
+
+
+                <img style={loaded ? { display: 'flex', alignSelf: 'center', height: "150%", width: "150%" } : { display: 'none' }}
+                     src={props.img}
+                     onLoad={() => setLoaded(true)}>
+                </img>
                 
-                
-                <img style={{ height: "100%", width: "100%"}} src={props.img}></img>
             </Modal>
+            
         </>
     )
 };
