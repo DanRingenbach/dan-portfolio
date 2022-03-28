646c9ab6dcc88330506f7e9a3cf541e026dc87fd
Daniel Ringenbach <danringenbach.art@gmail.com>
2022-03-28 15:09:43 -0400

    b&w changes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -14,7 +14,7 @@ export default function About() {
         </div>
         <div id="aboutmedescr">
           
-          <p className="m-5" style={{ textAlign: "left", fontSize: '25px'  }}>Dan Ringenbach is a front end developer as well as a UX/UI designer. He leverages his background in metal working and the fine arts to create web applications that feel sturdy and functional in addition to being light and responsive. He enjoys discussions on new design practices and methodologies with friends and associates and is always ready to play a friendly game of chess.</p>
+          <p className="m-5" style={{ textAlign: "left", fontSize: '25px', fontStyle: 'italic'  }}>Dan Ringenbach is a front end developer as well as a UX/UI designer. He leverages his background in metal working and the fine arts to create web applications that feel sturdy and functional in addition to being light and responsive. He enjoys discussions on new design practices and methodologies with friends and associates and is always ready to play a friendly game of chess.</p>
           <div className='m-3 contact '>
             <a href="mailto:danringenbach.art@gmail.com" >
               <button className="button">
