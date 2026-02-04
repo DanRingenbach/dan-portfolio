3a2a59dda50ccafea80fef04dce64f3d94778cb9
Dan Ringenbach <dan.ringenbach@penske.com>
2026-02-04 17:03:15 -0500

    Updates

--- _next/static/MlhdxpE9WbpnivQZ-Bkuk/_buildManifest.js
+++ _next/static/MlhdxpE9WbpnivQZ-Bkuk/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/dan-portfolio//_next/:path+",
+        "destination": "/dan-portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
