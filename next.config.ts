8e76a580d2d78148d710f78711c9f683149644ca
Dan Ringenbach <dan.ringenbach@penske.com>
2026-02-05 08:34:45 -0500

    \Switch from GitHub Pages to Vercel deployment"

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from 'next'
-
-const isProd = process.env.NODE_ENV === 'production';
  
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: isProd ? '/dan-portfolio' : '',
-  assetPrefix: isProd ? '/dan-portfolio/' : '',
   images: {
-    unoptimized: true, // GitHub Pages does not support Next.js image optimization
+    unoptimized: true,
   },
 }
  
